720493220351cbcca18651c3f74955c8668b7f2e
Alla <72519940+Alla183@users.noreply.github.com>
2026-03-16 09:05:58 +0200

    Update DoramaLandProvider.kt

--- DoramaLandProviders/src/main/kotlin/com/hexated/DoramaLandProvider.kt
+++ DoramaLandProviders/src/main/kotlin/com/hexated/DoramaLandProvider.kt
@@ -86,7 +86,7 @@ class DoramaLandProvider : MainAPI() {
        }
  }
 
-   override suspend fun loadLinks(
+override suspend fun loadLinks(
     data: String,
     isCasting: Boolean,
     subtitleCallback: (SubtitleFile) -> Unit,
@@ -95,36 +95,45 @@ class DoramaLandProvider : MainAPI() {
 
     val document = app.get(data).document
 
-    val iframe = document.selectFirst("#lazy-player2")?.attr("src")
-        ?: return false
+    val episode =
+        Regex("(\\d+)").find(data)?.groupValues?.getOrNull(1) ?: "1"
 
-    val iframeUrl = fixUrl(iframe)
+    val voices = document.select("#filterV option")
 
-    val player = app.get(iframeUrl).text
+    voices.forEach { voice ->
 
-    val m3u8 = Regex("""https://s\d+\.jaswish\.com[^\s"']+index\.m3u8""")
-        .find(player)
-        ?.value
+        val voiceId = voice.attr("value")
+        val voiceName = voice.text()
 
-    if (m3u8 != null) {
+        val iframeUrl =
+            "https://a.jaswish.com/pkybuen7l6vw5ars?v=$voiceId&s=$episode"
 
-        callback.invoke(
-            newExtractorLink(
-                "DoramaLand",
-                "DoramaLand",
-                m3u8,
-                ExtractorLinkType.M3U8
-            ) {
-                this.quality = Qualities.P1080.value
-                this.referer = "https://a.jaswish.com/"
-                this.headers = mapOf(
-                    "Origin" to "https://a.jaswish.com",
-                    "Referer" to "https://a.jaswish.com/"
-                )
-            }
-        )
+        val player = app.get(iframeUrl).text
+
+        val m3u8 =
+            Regex("""https://s\d+\.jaswish\.com[^\s"']+index\.m3u8""")
+                .find(player)
+                ?.value
+
+        if (m3u8 != null) {
+
+            callback.invoke(
+                newExtractorLink(
+                    "DoramaLand",
+                    voiceName,
+                    m3u8,
+                    ExtractorLinkType.M3U8
+                ) {
+                    this.referer = "https://a.jaswish.com/"
+                    this.headers = mapOf(
+                        "Origin" to "https://a.jaswish.com",
+                        "Referer" to "https://a.jaswish.com/"
+                    )
+                }
+            )
+        }
     }
 
     return true
-   }
+}
 }
