24ffabc4566d4c24478b49d4c75c8d96b7fae7be
Alla <72519940+Alla183@users.noreply.github.com>
2026-03-16 07:59:31 +0200

    Create build.gradle.kts

---
+++ DoramaLandProviders/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "uk"
+    description = "DoramaLand - Doramas and Asian series"
+    authors = listOf("Alla183")
+
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "AsianDrama"
+    )
+
+    iconUrl = "https://dorama.land/favicon.ico"
+}
