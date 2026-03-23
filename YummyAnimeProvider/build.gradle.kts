81363faa6d38b481dba83ab4c37da9ead8c88694
Alla <72519940+Alla183@users.noreply.github.com>
2026-03-23 09:50:24 +0200

    Create build.gradle.kts

---
+++ YummyAnimeProvider/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1
+
+cloudstream {
+    language = "uk"
+    description = "YummyAnime - Anime"
+    authors = listOf("Alla183")
+
+    status = 1
+
+    tvTypes = listOf("Anime")
+
+    iconUrl = "https://yummyanime.tv/favicon.ico"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
