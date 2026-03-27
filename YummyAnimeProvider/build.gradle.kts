86c90c25f9e09028f8abbf664e9a8a332391df62
Alla <72519940+Alla183@users.noreply.github.com>
2026-03-27 13:41:43 +0200

    Update build.gradle.kts

--- YummyAnimeProvider/build.gradle.kts
+++ YummyAnimeProvider/build.gradle.kts
@@ -1,24 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
+}
+
 version = 5
 
 android {
     buildFeatures {
         buildConfig = true
     }
-} 
+}
 
 cloudstream {
     language = "ru"
-    
 
-    
-     authors = listOf("Alla183")
-
+    authors = listOf("Alla183")
     status = 1 // will be 3 if unspecified
     tvTypes = listOf("Anime")
-
     iconUrl = "https://site.yummyani.me/favicon.ico"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0") // последняя версия
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
