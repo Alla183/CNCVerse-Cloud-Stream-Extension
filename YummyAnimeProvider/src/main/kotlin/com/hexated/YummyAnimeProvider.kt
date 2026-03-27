d54bceeed8537879e52c226351bc6220ca5daea1
Alla <72519940+Alla183@users.noreply.github.com>
2026-03-27 11:03:50 +0200

    Update YummyAnimeProvider.kt

--- YummyAnimeProvider/src/main/kotlin/com/hexated/YummyAnimeProvider.kt
+++ YummyAnimeProvider/src/main/kotlin/com/hexated/YummyAnimeProvider.kt
@@ -56,12 +56,10 @@ class YummyAnimeProvider : MainAPI() {
 
         return doc.select("a.image-block").mapNotNull { element ->
             val link = element.attr("href")
-            val poster = element.select("img").attr("src")
+            val poster = element.selectFirst("img")?.attr("src") ?: ""
 
             val parent = element.parent()
-            val title = parent.select(".anime-title").text()
-
-            if (title.isNullOrBlank()) return@mapNotNull null
+            val title = parent?.selectFirst(".anime-title")?.text() ?: return@mapNotNull null
 
             newAnimeSearchResponse(
                 title,
